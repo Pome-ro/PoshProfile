e900e55143a44d965b5a39a083a77728312cd77c
ParadoxCTRL <tom@pome.ro>
2017-11-25 18:51:11 -0500

    update profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,20 @@
-﻿# Config Script. This is full of PSDrives and other virables or path modifications that make life easy. 
-. $PSScriptRoot\config.ps1
+﻿. $PSScriptRoot\config.ps1
+Import-Module posh-git
 
 # Load Scripts
-Write-Host "ɸ Loading Scripts" -ForegroundColor Yellow
-Get-ChildItem coding:\Powershell\POSH-Profile\Scripts | Where {$_.Name -notlike "__*" -and $_.Name -like "*.ps1"} | ForEach { 
-    . $_.FullName 
-    
-}
-write-host ""
-import-module posh-git
-$ErrorActionPreference = "Stop"
 
 function Test-IsAdmin {
     ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 }
 
+
 # Set Prompt
-function prompt {
-    Write-Host "PowerShell " -NoNewline -ForegroundColor Cyan
-    if(Test-IsAdmin){
-        Write-Host "ɸ " -NoNewline -ForegroundColor red
-    } else {
-        Write-Host "ɸ " -NoNewline -ForegroundColor green
-    }
-    write-host (Get-Location).path -NoNewline
-
-    write-host " <" -NoNewline
-
- # Adds Git Folder features
+function prompt{
+    $loc =  (get-item -path .\).fullname
+    
+    # Display Admin if running as Admin
+    
     if(get-gitdirectory){
-        write-host " "
         $gitStatus = Get-Location | Get-Item | Get-Gitstatus
         $gitBranch = $gitStatus.Branch
         $gitUpstream = $gitStatus.Upstream
@@ -37,25 +22,24 @@ function prompt {
         $gitBehind = $gitStatus.Behindby
         $gitWorking = $gitStatus.Working
         $gitIndex = $gitStatus.Index
-        $primaryColor = "darkgray"
+        $primaryColor = "gray"
 
 
-        write-host "[BRANCH] " -BackgroundColor $primaryColor -ForegroundColor white -NoNewline
-        write-host "$gitBranch | " -BackgroundColor $primaryColor -ForegroundColor White -NoNewline
+        write-host "[|/]: $gitBranch " -ForegroundColor $primaryColor -NoNewline
         if($gitUpstream -ne $null){
-            write-host "[UP] $gitUpstream " -BackgroundColor $primaryColor -ForegroundColor White -NoNewline
+            write-host "[^]: $gitUpstream " -ForegroundColor $primaryColor -NoNewline
         }
 
         $AllStatus = @()
 
         if($gitStatus.hasWorking -eq $True){
-            $workingProp = @{total=$($gitWorking.length);color="DarkRed";symbol="X";name="Unstaged:"}
+            $workingProp = @{total=$($gitWorking.length);color="DarkRed";symbol="?";name="Unstaged:"}
             $Working = new-object -TypeName PSObject -Property $workingProp
             $Allstatus += $Working
         }
 
         if($gitStatus.hasIndex -eq $True){
-            $indexProp = @{total=$($gitIndex.length);color="DarkGreen";symbol="*";name="Staged:"}
+            $indexProp = @{total=$($gitIndex.length);color="DarkGreen";symbol="!";name="Staged:"}
             $Index = new-object -TypeName PSObject -Property $indexProp
             $Allstatus += $Index
         }
@@ -74,22 +58,19 @@ function prompt {
         if($allstatus.length -ne 0){
             for($g=0; $g -lt $allstatus.length; $g++){
                 $curItem = $allstatus[$g]
-                $nextItem = $allstatus[$g + 1]
-                $preItem = $allstatus[$g - 1]
-                if($g -eq 0){
-                    write-host "" -ForegroundColor DarkGray -BackgroundColor $curitem.color -NoNewline
-                } else {
-                    write-host "" -ForegroundColor $preitem.color -BackgroundColor $curitem.color -NoNewline
-                }
-                write-host " $($curitem.symbol) $($curitem.name) $($curitem.total) " -BackgroundColor $curitem.color -nonewline
-                if($($g+1) -eq $allstatus.length){
-                    write-host "" -ForegroundColor $curitem.color -NoNewline
-                }
+                write-host " $($curitem.symbol) $($curitem.name) $($curitem.total) " -ForegroundColor $curitem.color -nonewline
             }
-        } else {
-            write-host "" -ForegroundColor DarkGray -NoNewline
         }
+        write-host ""
     } 
-    write-host " "
+    if(Test-IsAdmin){
+        Write-Host "[A]" -NoNewline -BackgroundColor Black -ForegroundColor red
+    }
+    Write-Host "$loc" -ForegroundColor DarkGray
+    Write-Host ">" -ForegroundColor Cyan -NoNewline
+    Write-Host ">" -ForegroundColor Green -NoNewline
+    Write-Host ">" -ForegroundColor Yellow -NoNewline
+    Write-Host ">" -ForegroundColor Red -NoNewline
+
     Return " "
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# POSH Profile
+![](screenshot.jpg)
 
-define a config.ps1 file to set any psdrive mappings or credential storing.
+# POSH Profile
+My Powershell Profile. Has Git support for Git repositories. Define a config.ps1 file in the root folder with the profile to add any PSDrives, Import Scripts, etc. 
+
+* Font Size: `18`
+* Font Name: `Lucida Console`
+* Screen Background: `RGB(50,50,50)`
